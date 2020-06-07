7315e2f607caacb2b5e774d7e7ffdfd366bf0d26
Vievk Bhadra <vivek.bhadra@gmail.com>
2020-06-07 10:24:18 +0100

    Python program to find if a number is prime
    Python program to find if a number is prime

---
+++ prime_number.py
@@ -0,0 +1,26 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def check_prime(num):
+    if not isinstance(num,int):
+        return -1
+    else: 
+        for div in range(2, num//2 + 1):
+            if num % div == 0:
+                return 0
+    return 1
+
+def main():
+    ret = check_prime(53)
+    if  ret < 0:
+        print('not a valid number')
+    elif ret == 0:
+        print('not prime')
+    else:
+        print('prime')
+    
+if __name__ == '__main__':
+    main()
